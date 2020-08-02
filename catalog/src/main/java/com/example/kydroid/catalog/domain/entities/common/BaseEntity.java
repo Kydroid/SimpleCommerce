0bd445a16ca51937c0fa9d7c8df6e8f80ff51b32
Kydroid <yalmahdaoui@gmail.com>
2020-08-02 20:53:09 +0200

    Add a common BaseEntity class for all entities

---
+++ catalog/src/main/java/com/example/kydroid/catalog/domain/entities/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.kydroid.catalog.domain.entities.common;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isAlreadyPersisted() {
+        return id != null;
+    }
+}
