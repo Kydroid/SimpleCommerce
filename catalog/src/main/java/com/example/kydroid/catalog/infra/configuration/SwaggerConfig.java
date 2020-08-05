a9579d3393cfc0a7a5b63775f3d01ebb88ee123d
Kydroid <yalmahdaoui@gmail.com>
2020-08-05 18:36:40 +0200

    Prepare swagger for futur controllers

---
+++ catalog/src/main/java/com/example/kydroid/catalog/infra/configuration/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.example.kydroid.catalog.infra.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+// url access swagger = http://127.0.0.1:8080/api/v1.0/swagger-ui.html
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.kydroid.catalog"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
