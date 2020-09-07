a016b3f4ce0b9f8bd13b65cb4158a3b083cb107b
Kydroid <yalmahdaoui@gmail.com>
2020-09-07 23:54:32 +0200

    Prepare template demo - Add component footer demo static

---
+++ simple-commerce-ui/src/components/pages/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Footer extends React.Component<any, any> {
+    render() {
+        return (
+            <footer className="py-5 bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">Copyright © Your Website 2020</p>
+                </div>
+            </footer>
+        );
+    }
+
+}
