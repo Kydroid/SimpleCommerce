018ac552398095f00e9674fdf7fc3e10d07278f4
Kydroid <yalmahdaoui@gmail.com>
2020-09-10 23:32:55 +0200

    Get categories for category menu

--- simple-commerce-ui/src/components/category/CategoryMenu.tsx
+++ simple-commerce-ui/src/components/category/CategoryMenu.tsx
@@ -1,14 +1,36 @@
 import React from "react";
+import Axios from "axios";
+import {Category} from "../../entities/category";
 
 export class CategoryMenu extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            categories: []
+        };
+    }
+
+    componentDidMount() {
+        Axios.get("http://localhost:8080/api/v1.0/categories")
+            .then(res => {
+                const categories = res.data;
+                this.setState({
+                    categories: categories
+                })
+            })
+    }
+
     render() {
         return (
             <aside id="category-menu" className="col-lg-3">
                 <h1 className="my-4">Shop Name</h1>
                 <div className="list-group">
-                    <a className="list-group-item">Category 1</a>
-                    <a className="list-group-item">Category 2</a>
-                    <a className="list-group-item">Category 3</a>
+                    {
+                        this.state.categories.map((category: Category) =>
+                            <a key={category.id} className="list-group-item">
+                                {category.title}
+                            </a>)
+                    }
                 </div>
             </aside>
         );
