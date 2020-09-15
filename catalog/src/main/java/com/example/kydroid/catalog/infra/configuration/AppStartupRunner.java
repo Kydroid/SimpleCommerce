8ac1dbb3f087812a0d8b75bc8506c6e8601a1cae
Kydroid <yalmahdaoui@gmail.com>
2020-09-15 02:22:03 +0200

    Dont load default products/categories when test

--- catalog/src/main/java/com/example/kydroid/catalog/infra/configuration/AppStartupRunner.java
+++ catalog/src/main/java/com/example/kydroid/catalog/infra/configuration/AppStartupRunner.java
@@ -9,12 +9,14 @@ import com.example.kydroid.catalog.domain.ports.input.CreateProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+@Profile("!test")
 @Component
 public class AppStartupRunner implements ApplicationRunner {
 
@@ -44,8 +46,11 @@ public class AppStartupRunner implements ApplicationRunner {
         Product product2 = new Product("ref2", "prod2", "prod2 desc", 20,
                 new BigDecimal(20), "USD", category2);
         Product product3 = new Product("ref3", "prod3");
+        product3.setCategory(category3);
         Product product4 = new Product("ref4", "prod4");
+        product4.setCategory(category3);
         Product product5 = new Product("ref5", "prod5");
+        product5.setCategory(category3);
         List<Product> products = Arrays.asList(product1, product2, product3, product4, product5);
 
 
